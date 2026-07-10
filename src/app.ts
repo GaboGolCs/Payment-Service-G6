872a242cdc0f3ede614af826f9c71d5a2b3db61c
HAKAGOD <lexanytheneko@gmail.com>
2026-07-09 20:54:06 -0400

    webhook

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import { env, assertRequiredEnv } from './config/env';
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 // Rutas
 app.use('/api/payments', paymentRoutes);
